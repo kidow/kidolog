7e5d23b92ecdf314207a3494128b6369bdec3441
kidow <wcgo2ling@hanmail.net>
2018-12-20 16:02:03 +0900

    prettier set

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
-const {
-  PORT: port = 4000,
-  HOST: host = '127.0.0.1'
-} = process.env
+const morgan = require('morgan')
+const { PORT: port = 4000, HOST: host = '127.0.0.1' } = process.env
 const connect = require('./models')
 
 const app = express()
 connect()
 
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.listen(port, () => {
-  console.log(`App listening on ${host} and PORT ${port}!`);
-});
+  console.log(`App listening on ${host} and PORT ${port}!`)
+})
