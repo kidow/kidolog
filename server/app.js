c813206f1bd21e42a8acc79800228e405f696d2e
kidow <wcgo2ling@hanmail.net>
2018-12-20 12:39:02 +0900

    mongoose set

--- server/app.js
+++ server/app.js
@@ -5,8 +5,10 @@ const {
   PORT: port = 4000,
   HOST: host = '127.0.0.1'
 } = process.env
+const connect = require('./models')
 
 const app = express()
+connect()
 
 app.listen(port, () => {
   console.log(`App listening on ${host} and PORT ${port}!`);
