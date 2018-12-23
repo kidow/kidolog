f670d677a9b7a2939766fe1888a56f38b24e24ee
kidow <wcgo2ling@hanmail.net>
2018-12-23 16:20:51 +0900

    header ui v0.5(?) set

--- client/src/components/organisms/Header/Header.js
+++ client/src/components/organisms/Header/Header.js
@@ -4,7 +4,12 @@ import './Header.scss'
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__content">Kidolog</div>
+      <div className="header__content">
+        <div className="header__logo">
+          <a href="/">Kidolog</a>
+        </div>
+        <div className="header__buttons">버튼</div>
+      </div>
       <div className="header__divider" />
     </div>
   )
