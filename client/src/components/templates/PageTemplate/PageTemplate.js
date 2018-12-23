79c4e92f74e5ee1b113564f92b4c0f94596da91a
kidow <wcgo2ling@hanmail.net>
2018-12-23 14:48:50 +0900

    header simple set

--- client/src/components/templates/PageTemplate/PageTemplate.js
+++ client/src/components/templates/PageTemplate/PageTemplate.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './PageTemplate.scss'
+import { Header } from 'components/organisms'
 
 const PageTemplate = ({ children }) => {
-  return <div className="page-template">{children}</div>
+  return (
+    <div className="page-template">
+      <Header />
+      {children}
+    </div>
+  )
 }
 
 export default PageTemplate
