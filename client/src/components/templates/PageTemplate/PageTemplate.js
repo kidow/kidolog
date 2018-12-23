ff5796b122c95981ac2eac8dec85381ef3d0b6a8
kidow <wcgo2ling@hanmail.net>
2018-12-23 14:52:27 +0900

    footer simple set

--- client/src/components/templates/PageTemplate/PageTemplate.js
+++ client/src/components/templates/PageTemplate/PageTemplate.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './PageTemplate.scss'
-import { Header } from 'components/organisms'
+import { Header, Footer } from 'components/organisms'
 
 const PageTemplate = ({ children }) => {
   return (
     <div className="page-template">
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }
