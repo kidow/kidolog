0e9b82dc0f7321246103501b485530e6db58e0b9
kidow <wcgo2ling@hanmail.net>
2018-12-24 00:43:02 +0900

    markdown ui set

--- client/src/components/molecules/Markdown/Markdown.js
+++ client/src/components/molecules/Markdown/Markdown.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Markdown.scss'
+import { Input } from 'components/atoms'
 
 const Markdown = () => {
-  return <div>Markdown</div>
+  return (
+    <div className="markdown__container">
+      <div className="markdown__code-editor" />
+      <div className="markdown__tags">
+        <div className="markdown__description">태그</div>
+        <Input name="tags" placeholder="태그를 입력하세요 (쉼표로 구분)" />
+      </div>
+    </div>
+  )
 }
 
 export default Markdown
