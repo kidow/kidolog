39c11794cd45c2f792c949fd9f7f08cc537e91ae
kidow <wcgo2ling@hanmail.net>
2018-12-23 17:42:40 +0900

    header footer 아직 미완성. 나중에

--- client/src/components/organisms/Footer/Footer.js
+++ client/src/components/organisms/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.scss'
 
 const Footer = () => {
-  return <div className="footer">Footer</div>
+  return <div className="footer">Powered By Kidow. Copyright (c) 2019, Kidolog</div>
 }
 
 export default Footer
