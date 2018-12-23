b2795bfd2dfbb1779ba009b7dda2575b6bcbe157
kidow <wcgo2ling@hanmail.net>
2018-12-23 15:04:45 +0900

    postpage, editorpage, notfoundpage simple set

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { HomePage } from 'components/pages'
+import { HomePage, PostPage, EditorPage, NotFoundPage } from 'components/pages'
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/page/:page" component={HomePage} />
+        <Route path="/tag/:tag/:page?" component={HomePage} />
+        <Route path="/post/:id" component={PostPage} />
+        <Route path="/editor" component={EditorPage} />
+        <Route component={NotFoundPage} />
       </Switch>
     )
   }
--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,1 +1,4 @@
 export { default as HomePage } from './HomePage'
+export { default as EditorPage } from './EditorPage'
+export { default as PostPage } from './PostPage'
+export { default as NotFoundPage } from './NotFoundPage'
