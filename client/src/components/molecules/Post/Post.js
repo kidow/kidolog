ee91235f4f36bc31dc0c515101db93f0382de8f1
kidow <wcgo2ling@hanmail.net>
2018-12-23 19:51:13 +0900

    post ui basic set

--- client/src/components/molecules/Post/Post.js
+++ client/src/components/molecules/Post/Post.js
@@ -4,8 +4,11 @@ import './Post.scss'
 const Post = () => {
   return (
     <div className="post__container">
-      <div className="post__image">image</div>
-      <div className="post__title">title</div>
+      <div className="post__image">이미지가 없습니다</div>
+      <div className="post__title">
+        <div className="title__text">그동안의 개발을 하면서 느꼈던 점들</div>
+        <div className="title__date">2018-12-23</div>
+      </div>
       <div className="post__description">desc</div>
     </div>
   )
