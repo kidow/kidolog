005e08171d6ead43be299f3d43440cdaa0be0ba3
kidow <wcgo2ling@hanmail.net>
2018-12-23 15:14:54 +0900

    login page simple set

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { HomePage, PostPage, EditorPage, NotFoundPage } from 'components/pages'
+import LoginPage from './pages/LoginPage'
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
         <Route path="/page/:page" component={HomePage} />
         <Route path="/tag/:tag/:page?" component={HomePage} />
         <Route path="/post/:id" component={PostPage} />
