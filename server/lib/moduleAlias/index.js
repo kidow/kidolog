d949bf1d10093e8defbc062f623601a983d6163a
kidow <wcgo2ling@hanmail.net>
2018-12-25 00:45:52 +0900

    custom error set

--- server/lib/moduleAlias/index.js
+++ server/lib/moduleAlias/index.js
@@ -3,5 +3,6 @@ const moduleAlias = require('module-alias')
 moduleAlias.addAliases({
   '@lib': __dirname + '/../../lib',
   '@middle': __dirname + '/../../lib/middlewares',
-  '@models': __dirname + '/../../models'
+  '@models': __dirname + '/../../models',
+  '@error': __dirname + '/../../lib/error'
 })
