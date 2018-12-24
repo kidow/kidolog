d445b0bfe9108e02f107b88cd6a42f62259684e5
kidow <wcgo2ling@hanmail.net>
2018-12-24 23:54:55 +0900

    login input content post components ui re-set

--- client/src/components/organisms/Login/Login.js
+++ client/src/components/organisms/Login/Login.js
@@ -5,17 +5,19 @@ import { Input } from 'components/atoms'
 const Login = ({ password, onChangePassword, onKeyPress }) => {
   return (
     <div className="login__container">
-      <div className="login__input">
+      <div className="login__box">
         <h2>로그인</h2>
-        <Input
-          onChange={onChangePassword}
-          value={password}
-          placeholder="비밀번호 입력"
-          type="password"
-          autoFocus
-          onKeyPress={onKeyPress}
-          theme="login"
-        />
+        <div className="input">
+          <Input
+            onChange={onChangePassword}
+            value={password}
+            placeholder="비밀번호 입력"
+            type="password"
+            autoFocus
+            onKeyPress={onKeyPress}
+            theme="login"
+          />
+        </div>
       </div>
     </div>
   )
