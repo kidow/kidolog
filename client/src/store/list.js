a106ec33c70988c9fb86a1640203a6b1d257744d
kidow <wcgo2ling@hanmail.net>
2018-12-24 12:26:27 +0900

    list redux set

--- client/src/store/list.js
+++ client/src/store/list.js
@@ -1,10 +1,42 @@
 import { createAction, handleActions } from 'redux-actions'
-import { Map } from 'immutable'
+import { Map, fromJS, List } from 'immutable'
+import { pender } from 'redux-pender'
+import * as api from 'lib/api/list'
 
-const ACTION = 'ACTION'
+const GET_LIST = 'list/GET_LIST'
+const NEXT_LIST = 'list/NEXT_LIST'
+const SHOW_NEXT_LIST = 'list/SHOW_NEXT_LIST'
 
-export const action = createAction(ACTION)
+export const getList = createAction(GET_LIST, api.getList)
+export const nextList = createAction(NEXT_LIST, api.nextList)
+export const showNextList = createAction(SHOW_NEXT_LIST)
 
-const initialState = Map({})
+const initialState = Map({
+  next: '',
+  data: List(),
+  nextData: List()
+})
 
-export default handleActions({}, initialState)
+export default handleActions(
+  {
+    ...pender({
+      type: GET_LIST,
+      onSuccess: (state, action) => {
+        const { next, data } = action.payload.data
+        return state.set('next', next).set('data', fromJS(data))
+      }
+    }),
+    ...pender({
+      type: NEXT_LIST,
+      onSuccess: (state, action) => {
+        const { next, data } = action.payload.data
+        return state.set('next', next).set('nextData', fromJS(data))
+      }
+    }),
+    [SHOW_NEXT_LIST]: (state, action) => {
+      const nextData = state.get('nextData')
+      return state.update('data', data => data.concat(nextData)).set('nextData', List())
+    }
+  },
+  initialState
+)
