b8d2331cb3a249ece7dfb78382a2de2828aa76e9
kidow <wcgo2ling@hanmail.net>
2018-12-24 19:10:04 +0900

    auth router simple set

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
+const auth = require('./auth')
 const posts = require('./posts')
 
+app.use('/auth', auth)
 app.use('/posts', posts)
 
 module.exports = router
