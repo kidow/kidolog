8a70262c0bdf78cee581b59864be66929afe8fb6
kidow <wcgo2ling@hanmail.net>
2018-12-24 12:12:31 +0900

    api post set

--- client/src/lib/api/post.js
+++ client/src/lib/api/post.js
@@ -1,1 +1,6 @@
 import axios from 'axios'
+
+export const getPost = id => axios.get(`/posts/${id}`)
+export const updatePost = ({ id, title, markdown, tags }) =>
+  axios.patch(`/posts/${id}`, { title, markdown, tags })
+export const removePost = id => axios.delete(`/posts/${id}`)
