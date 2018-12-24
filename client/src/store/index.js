3ac237820626e4a59c508886ab5a755832eb8f0d
kidow <wcgo2ling@hanmail.net>
2018-12-24 10:33:12 +0900

    auth redux simple set

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,7 @@
 import editor from './editor'
 import list from './list'
 import post from './post'
+import auth from './auth'
 
 import { combineReducers } from 'redux'
 import { penderReducer as pender } from 'redux-pender'
@@ -9,5 +10,6 @@ export default combineReducers({
   editor,
   list,
   post,
+  auth,
   pender
 })
