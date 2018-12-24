3e5787f7008ad5a8ea25ce2c7fa01fa47ee0003a
kidow <wcgo2ling@hanmail.net>
2018-12-24 20:51:37 +0900

    post router write basic set

--- server/routes/private/auth/login.js
+++ server/routes/private/auth/login.js
@@ -1,5 +1,14 @@
+const Joi = require('joi')
+
 module.exports = async (req, res, next) => {
   const { password } = req.body
+
+  if (Joi.validate(password, Joi.string().required()).error) {
+    console.log(result.error)
+    res.sendStatus(400)
+    return
+  }
+
   if (password === process.env.PASSWORD) {
     req.session.logged = true
     res.json({ success: true })
--- server/routes/private/posts/write.js
+++ server/routes/private/posts/write.js
@@ -1,1 +1,26 @@
-module.exports = async (req, res, next) => {}
+const Post = require('@models/post')
+const Joi = require('joi')
+
+module.exports = async (req, res, next) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    markdown: Joi.string().required(),
+    tags: Joi.string().empty('')
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    console.log(result.error)
+    res.sendStatus(400)
+    return
+  }
+
+  try {
+    const post = await Post.write(req.body)
+    res.json(post)
+  } catch (e) {
+    console.error(e)
+    res.status(500)
+  }
+}
