18f8d8764fc2ee58cd78d2be8c80bc6cd39f0ac9
kidow <wcgo2ling@hanmail.net>
2018-12-24 20:01:33 +0900

    login router set

--- server/routes/private/auth/login.js
+++ server/routes/private/auth/login.js
@@ -1,1 +1,9 @@
-module.exports = async (req, res, next) => {}
+module.exports = async (req, res, next) => {
+  const { password } = req.body
+  if (password === process.env.PASSWORD) {
+    req.session.logged = true
+    res.json({ success: true })
+  } else {
+    next()
+  }
+}
