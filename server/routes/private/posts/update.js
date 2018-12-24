0862fb0d0f57c17d985bc6bd9b4bbd2f331b116b
kidow <wcgo2ling@hanmail.net>
2018-12-24 21:04:26 +0900

    post router update basic set

--- server/routes/private/posts/update.js
+++ server/routes/private/posts/update.js
@@ -1,1 +1,33 @@
-module.exports = async (req, res, next) => {}
+const Post = require('@models/post')
+
+module.exports = async (req, res, next) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    markdown: Joi.string().required(),
+    tags: Joi.string().empty('')
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    console.error(result.error)
+    res.sendStatus(400)
+    return
+  }
+
+  const { id } = req.params
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, req.body, {
+      new: true
+    }).exec()
+    if (!post) {
+      res.status(404)
+      res.json({ message: '404 Not Found' })
+    }
+    res.json(post)
+  } catch (e) {
+    res.status(500)
+    next(e)
+  }
+}
