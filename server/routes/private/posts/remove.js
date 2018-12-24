0d27205a331e11db3550db0e4e6885410adeb10f
kidow <wcgo2ling@hanmail.net>
2018-12-24 21:01:48 +0900

    post router remove basic set

--- server/routes/private/posts/remove.js
+++ server/routes/private/posts/remove.js
@@ -1,1 +1,12 @@
-module.exports = async (req, res, next) => {}
+const Post = require('@models/post')
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params
+  try {
+    await Post.findByIdAndRemove(id).exec()
+    res.status(204)
+  } catch (e) {
+    res.status(500)
+    next(e)
+  }
+}
