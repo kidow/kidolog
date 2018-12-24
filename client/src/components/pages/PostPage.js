7f1ba26631d8fa1e976d6142afcf3dcd896f7626
kidow <wcgo2ling@hanmail.net>
2018-12-24 14:37:40 +0900

    content container set. but not connected.

--- client/src/components/pages/PostPage.js
+++ client/src/components/pages/PostPage.js
@@ -3,11 +3,12 @@ import { PostTemplate } from 'components/templates'
 import { Content } from 'components/molecules'
 import { Logo } from 'components/atoms'
 
-const PostPage = () => {
+const PostPage = ({ match }) => {
+  const { id } = match.params
   return (
     <PostTemplate>
       <Logo big />
-      <Content />
+      <Content id={id} />
     </PostTemplate>
   )
 }
